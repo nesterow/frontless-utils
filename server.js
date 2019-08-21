2d19bde930fb28af8c7d47c148851f8ded8106d6
Anton Nesterov <arch.nesterov@gmail.com>
2019-08-21 11:41:39 +0300

    missed file

---
+++ server.js
@@ -0,0 +1,29 @@
+/*
+███████╗██████╗  ██████╗ ███╗   ██╗████████╗██╗     ███████╗███████╗███████╗
+██╔════╝██╔══██╗██╔═══██╗████╗  ██║╚══██╔══╝██║     ██╔════╝██╔════╝██╔════╝
+█████╗  ██████╔╝██║   ██║██╔██╗ ██║   ██║   ██║     █████╗  ███████╗███████╗
+██╔══╝  ██╔══██╗██║   ██║██║╚██╗██║   ██║   ██║     ██╔══╝  ╚════██║╚════██║
+██║     ██║  ██║╚██████╔╝██║ ╚████║   ██║   ███████╗███████╗███████║███████║
+╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚══════╝╚══════╝╚══════╝
+<<<<<<<<<<<<   FeathersJS - RiotJS - Turbolinks - Express    >>>>>>>>>>>>>>> 
+----------------------------------------------------------------------------
+@GitHub: https://github.com/nesterow/frontless
+@License: MIT
+@Author: Anton Nesterov <arch.nesterov@gmail.com>
+*/
+
+const babel = require('./src/babel')
+const render = require('./src/render')
+const Frontless = require('./src/middleware')
+
+const isBrowser = typeof window !== 'undefined'
+const {serializeForm} = require('./browser')
+
+module.exports = {
+  babel,
+  render,
+  Frontless,
+  isBrowser,
+  isServer: !isBrowser,
+  serializeForm
+}
